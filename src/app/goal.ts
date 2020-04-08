40cc4bee13f1099c55d41d5130ecf72b193a8226
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-08 12:57:46 +0300

    first commit

---
+++ src/app/goal.ts
@@ -0,0 +1,12 @@
+
+// export class Goal {
+//   id: number;
+//   name: string;
+//   description: string;
+// }
+export class Goal {
+    showDescription: boolean;
+    constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+      this.showDescription=false;
+    }
+  }
